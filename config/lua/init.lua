e4ec4814ac71a8be1401c19954b90da70750b856
Álan Crístoffer <acristoffers@startmail.com>
2023-10-24 15:08:59 +0200

    fix: respect --noplugin

--- config/lua/init.lua
+++ config/lua/init.lua
@@ -1,9 +1,13 @@
 vim.loader.enable()
 
-require("plugins.alpha")
-require("plugins.bufferline")
-require("plugins.lualine")
+if not vim.go.loadplugins then
+	vim.cmd([[se nu rnu]])
+else
+	require("plugins.alpha")
+	require("plugins.bufferline")
+	require("plugins.lualine")
 
-require("config.autocommands")
-require("config.keybindings")
-require("config.options")
+	require("config.autocommands")
+	require("config.keybindings")
+	require("config.options")
+end
