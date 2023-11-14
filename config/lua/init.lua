cd7db5a6b14ad052cd744737b144000241442044
Álan Crístoffer <acristoffers@startmail.com>
2023-11-14 11:19:06 +0100

    fix: plugins not loading

--- config/lua/init.lua
+++ config/lua/init.lua
@@ -1,13 +1,24 @@
 vim.loader.enable()
 
 if not vim.go.loadplugins then
-	vim.cmd([[se nu rnu]])
+    vim.cmd([[se nu rnu]])
+    vim.cmd([[se tabstop=8]])
+    vim.cmd([[se shiftwidth=4 smarttab expandtab]])
 else
-	require("plugins.alpha")
-	require("plugins.bufferline")
-	require("plugins.lualine")
+    require("plugins.alpha")
+    require("plugins.bufferline")
+    require("plugins.lualine")
 
-	require("config.autocommands")
-	require("config.keybindings")
-	require("config.options")
+    require("config.autocommands")
+    require("config.keybindings")
+    require("config.options")
+
+    require("plugins.other")
+    require("plugins.autopairs")
+    require("plugins.cmp")
+    require("plugins.comments")
+    require("plugins.gitsigns")
+    require("plugins.lsp")
+    require("plugins.luasnip")
+    require("plugins.treesitter")
 end
