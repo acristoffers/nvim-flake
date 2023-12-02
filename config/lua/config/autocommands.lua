c1f31f4f0e195a6b06052fb5397a9b8b54400110
Álan Crístoffer <acristoffers@startmail.com>
2023-12-02 12:19:10 +0100

    feat: defered configuration
    This allows neovim to process the UI before configuring some chunk of plugins, making it so
    the file is visible while the plugins are loading. The user still cannot interact with neovim
    in during the loading, but having the contents displayed allows me to start scanning the file
    to decide where to jump to while it happens. It gives only the impression of a fast startup,
    but I find it better than looking at a blank screen for half a second in some cases. At least
    I have information to start thinking about my next step while it loads.

--- config/lua/config/autocommands.lua
+++ config/lua/config/autocommands.lua
@@ -1,143 +1,136 @@
 local groups = {
-	initialization = {
-		{
-			event = { "BufRead", "BufNewFile" },
-			options = {
-				pattern = "*.zon",
-				callback = function()
-					vim.cmd([[set filetype=zig]])
-				end,
-			},
-		},
-		{
-			event = "VimEnter",
-			options = {
-				callback = function()
-					vim.defer_fn(function()
-						require("plugins.telescope")
-						require("plugins.whichkey")
-					end, 500)
-				end,
-			},
-		},
-	},
-	by_filetype = {
-		{
-			event = "FileType",
-			options = {
-				pattern = { "flutter", "dart" },
-				callback = function()
-					require("flutter-tools").setup({})
-				end,
-			},
-		},
-		{
-			event = "FileType",
-			options = {
-				pattern = { "rust" },
-				callback = function()
-					require("rust-tools").setup({})
-				end,
-			},
-		},
-		{
-			event = "FileType",
-			options = {
-				pattern = { "fish" },
-				callback = function()
-					vim.cmd([[
+    initialization = {
+        {
+            event = { "BufRead", "BufNewFile" },
+            options = {
+                pattern = "*.zon",
+                callback = function()
+                    vim.cmd([[set filetype=zig]])
+                end,
+            },
+        },
+    },
+    by_filetype = {
+        {
+            event = "FileType",
+            options = {
+                pattern = { "flutter", "dart" },
+                callback = function()
+                    vim.defer_fn(function()
+                        require("flutter-tools").setup({})
+                    end, 500)
+                end,
+            },
+        },
+        {
+            event = "FileType",
+            options = {
+                pattern = { "rust" },
+                callback = function()
+                    vim.defer_fn(function()
+                        require("rust-tools").setup({})
+                    end, 500)
+                end,
+            },
+        },
+        {
+            event = "FileType",
+            options = {
+                pattern = { "fish" },
+                callback = function()
+                    vim.cmd([[
                         compiler fish
                         setlocal textwidth=79
                         setlocal foldmethod=expr
                     ]])
-				end,
-			},
-		},
-	},
-	general = {
-		{
-			event = { "BufNewFile", "BufRead" },
-			options = {
-				pattern = { "*.m" },
-				command = "set filetype=matlab",
-			},
-		},
-		{
-			event = "FileType",
-			options = {
-				pattern = { "nix" },
-				command = "set commentstring=#\\ %s",
-			},
-		},
-		{
-			event = "FileType",
-			options = {
-				pattern = { "matlab" },
-				command = "set commentstring=\\%\\ %s",
-			},
-		},
-		{
-			event = "FileType",
-			options = {
-				pattern = { "qf", "help", "man", "lspinfo" },
-				command = "nnoremap <silent> <buffer> q :close<cr>",
-			},
-		},
-		{
-			event = "TextYankPost",
-			options = {
-				callback = function()
-					local hl_ok, hl = pcall(require, "vim.highlight")
-					if hl_ok then
-						hl.on_yank({ higroup = "Search", timeout = 200 })
-					else
-						print("Require error: no vim.highlight")
-					end
-				end,
-			},
-		},
-		{
-			event = "BufWinEnter",
-			options = { command = "set formatoptions-=cro" },
-		},
-		{
-			event = "FileType",
-			options = {
-				pattern = { "qf" },
-				command = "set nobuflisted",
-			},
-		},
-		{
-			event = "FileType",
-			options = {
-				pattern = { "gitcommit", "markdown" },
-				callback = function()
-					vim.cmd([[
+                end,
+            },
+        },
+    },
+    general = {
+        {
+            event = { "BufNewFile", "BufRead" },
+            options = {
+                pattern = { "*.m" },
+                command = "set filetype=matlab",
+            },
+        },
+        {
+            event = "FileType",
+            options = {
+                pattern = { "nix" },
+                command = "set commentstring=#\\ %s",
+            },
+        },
+        {
+            event = "FileType",
+            options = {
+                pattern = { "matlab" },
+                command = "set commentstring=\\%\\ %s",
+            },
+        },
+        {
+            event = "FileType",
+            options = {
+                pattern = { "qf", "help", "man", "lspinfo" },
+                command = "nnoremap <silent> <buffer> q :close<cr>",
+            },
+        },
+        {
+            event = "TextYankPost",
+            options = {
+                callback = function()
+                    local hl_ok, hl = pcall(require, "vim.highlight")
+                    if hl_ok then
+                        hl.on_yank({ higroup = "Search", timeout = 200 })
+                    else
+                        print("Require error: no vim.highlight")
+                    end
+                end,
+            },
+        },
+        {
+            event = "BufWinEnter",
+            options = { command = "set formatoptions-=cro" },
+        },
+        {
+            event = "FileType",
+            options = {
+                pattern = { "qf" },
+                command = "set nobuflisted",
+            },
+        },
+        {
+            event = "FileType",
+            options = {
+                pattern = { "gitcommit", "markdown" },
+                callback = function()
+                    vim.cmd([[
                         setlocal wrap
                         setlocal spell
                     ]])
-				end,
-			},
-		},
-	},
-	autoresize = {
-		{
-			event = "VimResized",
-			options = { command = "tabdo wincmd =" },
-		},
-	},
+                end,
+            },
+        },
+    },
+    autoresize = {
+        {
+            event = "VimResized",
+            options = { command = "tabdo wincmd =" },
+        },
+    },
 }
 
 for group, events in pairs(groups) do
-	local augroup = vim.api.nvim_create_augroup(group, { clear = true })
-	for _, event in pairs(events) do
-		local options = vim.tbl_extend("keep", { group = augroup }, event.options)
-		vim.api.nvim_create_autocmd(event.event, options)
-	end
+    local augroup = vim.api.nvim_create_augroup(group, { clear = true })
+    for _, event in pairs(events) do
+        local options = vim.tbl_extend("keep", { group = augroup }, event.options)
+        vim.api.nvim_create_autocmd(event.event, options)
+    end
 end
 
 vim.cmd([[
-" Save current view settings on a per-window, per-buffer basis.
+"Save current view settings on a per-window, per-buffer basis.
 function! AutoSaveWinView()
     if !exists("w:SavedBufView")
         let w:SavedBufView = {}
