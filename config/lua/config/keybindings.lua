ac0faea8c549e639cf26f179d8e9ccf2f8c0974e
Álan Crístoffer <acristoffers@startmail.com>
2024-09-07 21:40:08 +0200

    feat: add keybindings for ledger

--- config/lua/config/keybindings.lua
+++ config/lua/config/keybindings.lua
@@ -4,9 +4,9 @@ local opts = { noremap = true, silent = true }
 local keymap = vim.keymap.set
 
 --Remap space as leader key
-keymap('', '<Space>', '<Nop>', opts)
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+keymap("", "<Space>", "<Nop>", opts)
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
 -- Modes
 --   normal_mode = 'n',
@@ -18,130 +18,112 @@ vim.g.maplocalleader = ' '
 
 -- Normal --
 
-keymap('n', 'gp', '`<v`>', opts)
+keymap("n", "gp", "`<v`>", opts)
 
-keymap('n', '<A-p>', function() vim.fn.setreg('+', vim.api.nvim_buf_get_name(0)) end, opts)
+keymap("n", "<A-p>", function()
+  vim.fn.setreg("+", vim.api.nvim_buf_get_name(0))
+end, opts)
 
 -- Resize with arrows
-keymap('n', '<C-Up>', ':resize +2<cr>', opts)
-keymap('n', '<C-Down>', ':resize -2<cr>', opts)
-keymap('n', '<C-Left>', ':vertical resize -2<cr>', opts)
-keymap('n', '<C-Right>', ':vertical resize +2<cr>', opts)
+keymap("n", "<C-Up>", ":resize +2<cr>", opts)
+keymap("n", "<C-Down>", ":resize -2<cr>", opts)
+keymap("n", "<C-Left>", ":vertical resize -2<cr>", opts)
+keymap("n", "<C-Right>", ":vertical resize +2<cr>", opts)
 
 -- "Tabs" navigation
-keymap('n', 'gt', ':bn<cr>', opts)
-keymap('n', 'gT', ':bp<cr>', opts)
+keymap("n", "gt", ":bn<cr>", opts)
+keymap("n", "gT", ":bp<cr>", opts)
 
 -- Snipe last
-keymap('n', '<A-g>', '$,', opts)
+keymap("n", "<A-g>", "$,", opts)
 
 -- Snipe first
-keymap('n', '<A-f>', '0;', opts)
+keymap("n", "<A-f>", "0;", opts)
 
 -- Insert --
 -- Press jk fast to enter
-keymap('i', 'jk', '<ESC>', opts)
+keymap("i", "jk", "<ESC>", opts)
 
 -- Visual --
 -- Stay in indent mode
-keymap('v', '<', '<gv', opts)
-keymap('v', '>', '>gv', opts)
+keymap("v", "<", "<gv", opts)
+keymap("v", ">", ">gv", opts)
 
 -- Move text up and down
-keymap('v', '<A-j>', ':m .+1<cr>==', opts)
-keymap('v', '<A-k>', ':m .-2<cr>==', opts)
-keymap('n', '<A-j>', ':m .+1<cr>==', opts)
-keymap('n', '<A-k>', ':m .-2<cr>==', opts)
+keymap("v", "<A-j>", ":m .+1<cr>==", opts)
+keymap("v", "<A-k>", ":m .-2<cr>==", opts)
+keymap("n", "<A-j>", ":m .+1<cr>==", opts)
+keymap("n", "<A-k>", ":m .-2<cr>==", opts)
 
 -- Navigate tags
-keymap('n', 'g[', ':pop<cr>', opts)
-keymap('n', 'g]', ':tag<cr>', opts)
+keymap("n", "g[", ":pop<cr>", opts)
+keymap("n", "g]", ":tag<cr>", opts)
 
-keymap('v', 'p', '"_dP', opts)
+keymap("v", "p", '"_dP', opts)
 
 -- Visual Block --
 -- Move text up and down
-keymap('x', '<A-j>', ':move \'>+1<cr>gv-gv', opts)
-keymap('x', '<A-k>', ':move \'<-2<cr>gv-gv', opts)
+keymap("x", "<A-j>", ":move '>+1<cr>gv-gv", opts)
+keymap("x", "<A-k>", ":move '<-2<cr>gv-gv", opts)
 
 -- Moving around buffers --
-keymap('n', '<A-1>', ':BufferLineGoToBuffer 1<cr>', opts)
-keymap('n', '<A-2>', ':BufferLineGoToBuffer 2<cr>', opts)
-keymap('n', '<A-3>', ':BufferLineGoToBuffer 3<cr>', opts)
-keymap('n', '<A-4>', ':BufferLineGoToBuffer 4<cr>', opts)
-keymap('n', '<A-5>', ':BufferLineGoToBuffer 5<cr>', opts)
-keymap('n', '<A-6>', ':BufferLineGoToBuffer 6<cr>', opts)
-keymap('n', '<A-7>', ':BufferLineGoToBuffer 7<cr>', opts)
-keymap('n', '<A-8>', ':BufferLineGoToBuffer 8<cr>', opts)
-keymap('n', '<A-9>', ':BufferLineGoToBuffer 9<cr>', opts)
+keymap("n", "<A-1>", ":BufferLineGoToBuffer 1<cr>", opts)
+keymap("n", "<A-2>", ":BufferLineGoToBuffer 2<cr>", opts)
+keymap("n", "<A-3>", ":BufferLineGoToBuffer 3<cr>", opts)
+keymap("n", "<A-4>", ":BufferLineGoToBuffer 4<cr>", opts)
+keymap("n", "<A-5>", ":BufferLineGoToBuffer 5<cr>", opts)
+keymap("n", "<A-6>", ":BufferLineGoToBuffer 6<cr>", opts)
+keymap("n", "<A-7>", ":BufferLineGoToBuffer 7<cr>", opts)
+keymap("n", "<A-8>", ":BufferLineGoToBuffer 8<cr>", opts)
+keymap("n", "<A-9>", ":BufferLineGoToBuffer 9<cr>", opts)
 
 -- Paste with motion --
-keymap('n', 'gP', 'PasteMotion()', { silent = true, expr = true })
-keymap('x', 'gP', 'PasteMotion()', { silent = true, expr = true })
+keymap("n", "gP", "PasteMotion()", { silent = true, expr = true })
+keymap("x", "gP", "PasteMotion()", { silent = true, expr = true })
 
 -- Centered screen movements --
-keymap('n', '<C-u>', '<C-u>zz', { silent = true, noremap = true })
-keymap('n', '<C-d>', '<C-d>zz', { silent = true, noremap = true })
-keymap('n', 'n', 'nzz', { silent = true, noremap = true })
+keymap("n", "<C-u>", "<C-u>zz", { silent = true, noremap = true })
+keymap("n", "<C-d>", "<C-d>zz", { silent = true, noremap = true })
+keymap("n", "n", "nzz", { silent = true, noremap = true })
 
 -- Horizontal center --
-keymap('n', 'z|', 'zszH', { silent = true, noremap = true })
+keymap("n", "z|", "zszH", { silent = true, noremap = true })
 
 -- Remove surrounding function call --
-keymap('n', '<space>dSF', 'yiabgPaF', { silent = true, noremap = false })
+keymap("n", "<space>dSF", "yiabgPaF", { silent = true, noremap = false })
 
 -- Text Object for the entire buffer --
-keymap('v', 'aG', ':<C-U>normal! ggVG$<cr>', { silent = true, noremap = true })
-keymap('v', 'iG', ':<C-U>normal! ggVG$<cr>', { silent = true, noremap = true })
-keymap('v', 'ag', ':<C-U>normal! ggVG$<cr>', { silent = true, noremap = true })
-keymap('v', 'ig', ':<C-U>normal! ggVG$<cr>', { silent = true, noremap = true })
-keymap('o', 'aG', ':normal VaG$<cr>', { silent = true, noremap = false })
-keymap('o', 'iG', ':normal ViG$<cr>', { silent = true, noremap = false })
-keymap('o', 'ag', ':normal VaG$<cr>', { silent = true, noremap = false })
-keymap('o', 'ig', ':normal ViG$<cr>', { silent = true, noremap = false })
+keymap("v", "aG", ":<C-U>normal! ggVG$<cr>", { silent = true, noremap = true })
+keymap("v", "iG", ":<C-U>normal! ggVG$<cr>", { silent = true, noremap = true })
+keymap("v", "ag", ":<C-U>normal! ggVG$<cr>", { silent = true, noremap = true })
+keymap("v", "ig", ":<C-U>normal! ggVG$<cr>", { silent = true, noremap = true })
+keymap("o", "aG", ":normal VaG$<cr>", { silent = true, noremap = false })
+keymap("o", "iG", ":normal ViG$<cr>", { silent = true, noremap = false })
+keymap("o", "ag", ":normal VaG$<cr>", { silent = true, noremap = false })
+keymap("o", "ig", ":normal ViG$<cr>", { silent = true, noremap = false })
 
 -- Text Object for Identifier (o in Doom Emacs) --
-keymap(
-  'v',
-  'ao',
-  ':lua SelectIdentifier()<cr>',
-  { silent = true, noremap = true }
-)
-keymap(
-  'v',
-  'io',
-  ':lua SelectIdentifier()<cr>',
-  { silent = true, noremap = true }
-)
-keymap(
-  'o',
-  'ao',
-  ':lua SelectIdentifier()<cr>',
-  { silent = true, noremap = true }
-)
-keymap(
-  'o',
-  'io',
-  ':lua SelectIdentifier()<cr>',
-  { silent = true, noremap = true }
-)
+keymap("v", "ao", ":lua SelectIdentifier()<cr>", { silent = true, noremap = true })
+keymap("v", "io", ":lua SelectIdentifier()<cr>", { silent = true, noremap = true })
+keymap("o", "ao", ":lua SelectIdentifier()<cr>", { silent = true, noremap = true })
+keymap("o", "io", ":lua SelectIdentifier()<cr>", { silent = true, noremap = true })
 
 -- LSP is having trouble defining those --
 local telescope = ':lua require"telescope.builtin"'
-keymap('n', 'gd', telescope .. '.lsp_definitions()<cr>', opts)
-keymap('n', 'gi', telescope .. '.lsp_implementations()<cr>', opts)
-keymap('n', 'gr', telescope .. '.lsp_references()<cr>', opts)
-keymap('n', 'gD', ':lua vim.lsp.buf.declaration()<cr>', opts)
-keymap('n', 'K', ':lua vim.lsp.buf.hover()<cr>', opts)
-keymap('n', '<C-k>', ':lua vim.lsp.buf.signature_help()<cr>', opts)
-keymap('n', '[d', ':lua vim.diagnostic.goto_prev()<cr>', opts)
-keymap('n', ']d', ':lua vim.diagnostic.goto_next()<cr>', opts)
+keymap("n", "gd", telescope .. ".lsp_definitions()<cr>", opts)
+keymap("n", "gi", telescope .. ".lsp_implementations()<cr>", opts)
+keymap("n", "gr", telescope .. ".lsp_references()<cr>", opts)
+keymap("n", "gD", ":lua vim.lsp.buf.declaration()<cr>", opts)
+keymap("n", "K", ":lua vim.lsp.buf.hover()<cr>", opts)
+keymap("n", "<C-k>", ":lua vim.lsp.buf.signature_help()<cr>", opts)
+keymap("n", "[d", ":lua vim.diagnostic.goto_prev()<cr>", opts)
+keymap("n", "]d", ":lua vim.diagnostic.goto_next()<cr>", opts)
 
 -- Center selection
-keymap('v', 'zZ', ':lua CenterSelection()<cr>', opts)
+keymap("v", "zZ", ":lua CenterSelection()<cr>", opts)
 
 --- Eval math (actually lua)
-keymap('v', '<leader>r', ':lua EvalLua()<rc>', opts)
+keymap("v", "<leader>r", ":lua EvalLua()<rc>", opts)
 
 function CenterSelection()
   local startPos = vim.fn.getpos("'<")
@@ -150,10 +132,10 @@ function CenterSelection()
   local endLine = endPos[2]
   local middleLine = math.floor((startLine + endLine) / 2)
   vim.api.nvim_win_set_cursor(0, { middleLine, 0 })
-  vim.cmd('normal! zz')
+  vim.cmd("normal! zz")
   vim.fn.setpos("'<", startPos)
   vim.fn.setpos("'>", endPos)
-  vim.cmd('normal! gv')
+  vim.cmd("normal! gv")
 end
 
 function EvalLua()
@@ -169,7 +151,7 @@ function SelectIdentifier()
   local len = string.len(vim.api.nvim_get_current_line())
   local s = vim.api.nvim_win_get_cursor(0)[2]
   local e = vim.api.nvim_win_get_cursor(0)[2] + 1
-  local regex = vim.regex '^[a-zA-Z0-9_-]\\+$'
+  local regex = vim.regex("^[a-zA-Z0-9_-]\\+$")
 
   while regex:match_line(0, line, s, e) do
     s = s - 1
@@ -187,19 +169,85 @@ function SelectIdentifier()
   end
   e = e - 3
 
-  vim.api.nvim_buf_set_mark(0, '<', line + 1, s, {})
-  vim.api.nvim_buf_set_mark(0, '>', line + 1, e, {})
-  vim.cmd [[ normal! gv ]]
+  vim.api.nvim_buf_set_mark(0, "<", line + 1, s, {})
+  vim.api.nvim_buf_set_mark(0, ">", line + 1, e, {})
+  vim.cmd([[ normal! gv ]])
 end
 
-vim.cmd [[
+vim.cmd([[
 function PasteMotion()
   set opfunc=v:lua.PasteMotion
   return 'g@'
 endfunction
-]]
+]])
 
 ---@diagnostic disable-next-line: unused-local
 function _G.PasteMotion(type)
-  vim.cmd [[ normal gpP ]]
+  vim.cmd([[ normal gpP ]])
+end
+
+function NamedNodeAroundCursor(node_name)
+  local parser = vim.treesitter.get_parser()
+  local tree = parser:trees()[1]
+  local root = tree:root()
+
+  local cursor = vim.api.nvim_win_get_cursor(0)
+  local cursor_row = cursor[1] - 1
+  local cursor_col = cursor[2]
+
+  local closest_node = root:named_descendant_for_range(cursor_row, cursor_col, cursor_row, cursor_col)
+
+  while closest_node and closest_node:id() ~= root:id() do
+    if closest_node:type() == node_name then
+      return closest_node
+    end
+    closest_node = closest_node:parent()
+  end
+
+  return nil
+end
+
+function NamedNodeAfterCursor(node_name)
+  local parser = vim.treesitter.get_parser()
+  local tree = parser:trees()[1]
+  local root = tree:root()
+
+  local cursor = vim.api.nvim_win_get_cursor(0)
+  local cursor_row = cursor[1] - 1
+  local cursor_col = cursor[2]
+
+  local function walk_children(node)
+    if node then
+      local sr, sc, _, _ = node:range()
+      local is_after = (sr > cursor_row) or (sr == cursor_row and sc > cursor_col)
+
+      if is_after and node:type() == node_name then
+        return node
+      end
+
+      for _, child in pairs(node:named_children()) do
+        local n = walk_children(child)
+        if n then
+          return n
+        end
+      end
+    end
+
+    return nil
+  end
+
+  return walk_children(root)
+end
+
+function NamedNodeSnipe(node_name)
+  local node = NamedNodeAroundCursor(node_name)
+  if node == nil then
+    node = NamedNodeAfterCursor(node_name)
+  end
+  if node then
+    local ls, cs, le, ce = node:range()
+    vim.api.nvim_buf_set_mark(0, "<", ls + 1, cs, {})
+    vim.api.nvim_buf_set_mark(0, ">", le + 1, ce - 2, {})
+    vim.cmd([[ normal! gv ]])
+  end
 end
