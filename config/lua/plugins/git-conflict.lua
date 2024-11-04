eac2fc26281642da0595a0f0ce4f3852f2779258
Álan e Sousa <alan.esousa@vitibot.fr>
2024-11-04 09:57:18 +0100

    feat: modify git conflict background colors

--- config/lua/plugins/git-conflict.lua
+++ config/lua/plugins/git-conflict.lua
@@ -1,12 +1,17 @@
 local gitconflict = require("git-conflict")
 
+vim.cmd([[
+  highlight DiffCurrent  guibg=#405d7e guifg=#50fa7b
+  highlight DiffIncoming guibg=#282136 guifg=#50fa7b
+]])
+
 gitconflict.setup({
   default_commands = true,       -- disable commands created by this plugin
   disable_diagnostics = false,   -- This will disable the diagnostics in a buffer whilst it is conflicted
   list_opener = "copen",         -- command or function to open the conflicts list
   highlights = {                 -- They must have background color, otherwise the default color will be used
-    incoming = "DiffAdd",
-    current = "DiffText",
+    current = "DiffCurrent",
+    incoming = "DiffIncoming",
   },
   default_mappings = {
     ours = "ch",
