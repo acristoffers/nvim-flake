62ada52586b191b5629681f3d0700661f39ec14c
Álan e Sousa <alan.esousa@vitibot.fr>
2024-11-06 13:55:51 +0100

    feat: configure firenvim to ignore the rgdi

--- config/lua/plugins/other.lua
+++ config/lua/plugins/other.lua
@@ -18,16 +18,16 @@ require("trim").setup({
 })
 require("markdown").setup({
   mappings = {
-    inline_surround_toggle = "<leader>mi", -- (string|boolean) toggle inline style
+    inline_surround_toggle = "<leader>mi",      -- (string|boolean) toggle inline style
     inline_surround_toggle_line = "<leader>mI", -- (string|boolean) line-wise toggle inline style
-    inline_surround_delete = "<leader>md", -- (string|boolean) delete emphasis surrounding cursor
-    inline_surround_change = "<leader>mc", -- (string|boolean) change emphasis surrounding cursor
-    link_add = "<leader>ml", -- (string|boolean) add link
-    link_follow = "<leader>mf", -- (string|boolean) follow link
-    go_curr_heading = "]c", -- (string|boolean) set cursor to current section heading
-    go_parent_heading = "]p", -- (string|boolean) set cursor to parent section heading
-    go_next_heading = "]]", -- (string|boolean) set cursor to next section heading
-    go_prev_heading = "[[", -- (string|boolean) set cursor to previous section heading
+    inline_surround_delete = "<leader>md",      -- (string|boolean) delete emphasis surrounding cursor
+    inline_surround_change = "<leader>mc",      -- (string|boolean) change emphasis surrounding cursor
+    link_add = "<leader>ml",                    -- (string|boolean) add link
+    link_follow = "<leader>mf",                 -- (string|boolean) follow link
+    go_curr_heading = "]c",                     -- (string|boolean) set cursor to current section heading
+    go_parent_heading = "]p",                   -- (string|boolean) set cursor to parent section heading
+    go_next_heading = "]]",                     -- (string|boolean) set cursor to next section heading
+    go_prev_heading = "[[",                     -- (string|boolean) set cursor to previous section heading
   },
   on_attach = function(bufnr)
     local map = vim.keymap.set
@@ -39,3 +39,20 @@ require("markdown").setup({
     map("n", "<leader>mt", ":MDToc<CR>", opts)
   end,
 })
+
+vim.g.firenvim_config = {
+  globalSettings = { alt = "all" },
+  localSettings = {
+    [".*"] = {
+      cmdline  = "neovim",
+      content  = "text",
+      priority = 0,
+      selector = "textarea",
+      takeover = "always"
+    },
+    [".*rgdi.vitibot.*"] = {
+      takeover = 'never',
+      priority = 1
+    }
+  }
+}
