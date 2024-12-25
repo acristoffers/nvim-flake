1a00134e062d53a8baee8caa596573f124af883d
Álan Crístoffer <acristoffers@startmail.com>
2024-12-25 21:17:28 +0100

    fix: do not check vim plugins in nix

--- config/lua/lsp/settings/bashls.lua
+++ config/lua/lsp/settings/bashls.lua
@@ -1,9 +1,12 @@
-local util = require 'lspconfig.util'
+local ok, util = pcall(require, 'lspconfig.util')
+if not ok then
+  return {}
+end
 
 return {
   cmd = { "bash-language-server", "start" },
   filetypes = { "sh", "bash", "zsh" },
-  root_dir = util.find_git_ancestor,
+    root_dir = util.find_git_ancestor,
   settings = {
     bashIde = {
       globPattern = "*@(.sh|.inc|.bash|.command|.zsh)"
