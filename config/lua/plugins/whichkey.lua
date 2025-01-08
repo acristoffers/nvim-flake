c432f252a996925de25057dc46c4e463b1551639
Álan e Sousa <alan.esousa@vitibot.fr>
2025-01-08 16:56:20 +0100

    feat: add inlay hint toggle

--- config/lua/plugins/whichkey.lua
+++ config/lua/plugins/whichkey.lua
@@ -111,11 +111,11 @@ local options = {
       nowait = true,
       remap = false,
     },
-    { "<leader>cc", ":TextCaseOpenTelescope<cr>", desc = "Change Case", nowait = true, remap = false },
-    { "<leader>ch", ":Ouroboros<cr>", desc = "Switch .h/.cpp", nowait = true, remap = false },
-    { "<leader>ct", ":Trim<cr>", desc = "Trim trailling spaces", nowait = true, remap = false },
-    { "<leader>cf", ":Format<cr>", desc = "Format (formatter.nvim)", nowait = true, remap = false },
-    { "<leader>l", group = "LSP", nowait = true, remap = false },
+    { "<leader>cc", ":TextCaseOpenTelescope<cr>", desc = "Change Case",             nowait = true, remap = false },
+    { "<leader>ch", ":Ouroboros<cr>",             desc = "Switch .h/.cpp",          nowait = true, remap = false },
+    { "<leader>ct", ":Trim<cr>",                  desc = "Trim trailling spaces",   nowait = true, remap = false },
+    { "<leader>cf", ":Format<cr>",                desc = "Format (formatter.nvim)", nowait = true, remap = false },
+    { "<leader>l",  group = "LSP",                nowait = true,                    remap = false },
     {
       "<leader>la",
       ":lua vim.lsp.buf.code_action()<cr>",
@@ -238,6 +238,15 @@ local options = {
       "<leader>te",
       ":NvimTreeToggle<CR>",
       desc = "File Tree",
+      nowait = true,
+      remap = false,
+    },
+    {
+      "<leader>ti",
+      function()
+        vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
+      end,
+      desc = "Inlay Hints",
       nowait = true,
       remap = false,
     },
@@ -812,10 +821,10 @@ end
 
 local specs = {
   mode = { "v" },
-  { "<leader>", group = "Text", nowait = true, remap = false },
-  { "<leader>s", ":sort<cr>", desc = "Sort", nowait = true, remap = false },
-  { "<leader>S", ":sort i<cr>", desc = "Sort (Case Insensitive)", nowait = true, remap = false },
-  { "<leader>u", ":!uniq <cr>", desc = "Unique", nowait = true, remap = false },
+  { "<leader>",  group = "Text", nowait = true,                    remap = false },
+  { "<leader>s", ":sort<cr>",    desc = "Sort",                    nowait = true, remap = false },
+  { "<leader>S", ":sort i<cr>",  desc = "Sort (Case Insensitive)", nowait = true, remap = false },
+  { "<leader>u", ":!uniq <cr>",  desc = "Unique",                  nowait = true, remap = false },
 }
 
 which_key.add(specs)
