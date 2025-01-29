6935ab8f4abe3d2c2a21a5321eec1a8f80b174d8
Álan Crístoffer e Sousa <acristoffers@startmail.com>
2025-01-29 09:16:01 +0100

    feat: support for GitLab's YAML !reference tag

---
+++ config/lua/lsp/settings/yamlls.lua
@@ -0,0 +1,19 @@
+return {
+  settings = {
+    yaml = {
+      hover = true,
+      -- format = {
+      --   enable = true,
+      --   singleQuote = true,
+      -- },
+      completion = true,
+      validate = true,
+      schemaStore = {
+        enable = true,
+      },
+      customTags = {
+        "!reference sequence"
+      },
+    },
+  }
+}
