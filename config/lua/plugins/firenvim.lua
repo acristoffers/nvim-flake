c676ac36f9b28a70f09c952fb4968abf669e33ca
Álan Crístoffer <acristoffers@startmail.com>
2025-05-01 09:09:01 +0200

    feature(firenvim): don't take over instagram

--- config/lua/plugins/firenvim.lua
+++ config/lua/plugins/firenvim.lua
@@ -1,26 +1,26 @@
-vim.g.firenvim_config = {
-  globalSettings = { alt = "all" },
-  localSettings = {
-    [".*rgdi.vitibot.*"] = {
-      takeover = "never",
-      priority = 1,
-    },
-    [".*search.brave.com"] = {
-      takeover = "never",
-      priority = 1,
-    },
-    [".*www.google.*"] = {
-      takeover = "never",
-      priority = 1,
-    },
-    [".*mastodon.*"] = {
-      takeover = "never",
-      priority = 1,
-    },
-  },
+local ignored_urls = {
+  ".*rgdi.vitibot.*",
+  ".*search.brave.com",
+  ".*www.google.*",
+  ".*mastodon.*",
+  ".*instagram.*"
 }
 
-vim.api.nvim_create_autocmd({'BufEnter'}, {
-    pattern = "git.vitibot.fr_*.txt",
-    command = "set filetype=markdown tw=70"
+local exceptions = {}
+
+for _, url in pairs(ignored_urls) do
+  exceptions[url] = {
+    takeover = "never",
+    priority = 1,
+  }
+end
+
+vim.g.firenvim_config = {
+  globalSettings = { alt = "all" },
+  localSettings = exceptions,
+}
+
+vim.api.nvim_create_autocmd({ "BufEnter" }, {
+  pattern = "git.vitibot.fr_*.txt",
+  command = "set filetype=markdown tw=70",
 })
