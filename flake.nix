e74baec24c1a005ee622a940d529926fb7ea27d9
Álan Crístoffer <acristoffers@startmail.com>
2026-02-05 12:04:13 +0100

    fix: markview is now on hybrid mode, fixing the conceal glitch

--- flake.nix
+++ flake.nix
@@ -467,9 +467,13 @@
                     config = function ()
                       require'markview'.setup {
                         preview = {
+                          edit_range = { 0, 0 },
                           filetypes = { "markdown", "codecompanion" },
-                          ignore_buftypes = {},
+                          hybrid_modes = { "n", "i" },
                           icon_provider = "mini",
+                          ignore_buftypes = {},
+                          linewise_hybrid_mode = true,
+                          modes = { "n", "i" },
                         }
                       }
                     end
