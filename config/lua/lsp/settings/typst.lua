0d722f058b288f74cc7d00fc8927e3b091ae45b3
Álan Crístoffer <acristoffers@startmail.com>
2026-03-24 10:08:06 +0100

    fix: add more root markers to tinymist
    Like this it can better detect the fonts folder.

--- config/lua/lsp/settings/typst.lua
+++ config/lua/lsp/settings/typst.lua
@@ -1,5 +1,7 @@
 return {
+  root_dir = require('lspconfig.util').root_pattern('.projectile', 'Makefile', '.git'),
   settings = {
+    rootPath = ".", -- interpreted relative to the chosen root_dir
     formatterMode = "typstyle",
     fontPaths = { "./fonts" },
   }
